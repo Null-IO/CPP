00b3be4912818a64ead28044930c78ba933167e2
Null-IO <z.gergaia@hotmail.com>
2020-08-06 01:00:39 +0400

    Update to Thread

--- Threads/future(async).cpp
+++ Threads/future(async).cpp
@@ -6,25 +6,25 @@
 #include <vector>
 using namespace std;
 
-static const size_t NUM = 10000000;
+static const size_t NUM = 100000000;
 
 long long getDotProduct(vector<long long>& v, vector<long long>& w) {
 	auto vSize = v.size();
 
 	auto future1 = async([&] {
-		return inner_product(v.begin(), v.begin() + (vSize / 4), w.begin(), 0);
+		return inner_product(v.begin(), v.begin() + (vSize / 4), w.begin(), 0LL);
 		});
 
 	auto future2 = async([&] {
-		return inner_product(v.begin() + (vSize / 4), v.begin() + (vSize / 2), w.begin() + (vSize / 4), 0);
+		return inner_product(v.begin() + (vSize / 4), v.begin() + (vSize / 2), w.begin() + (vSize / 4), 0LL);
 		});
 
 	auto future3 = async([&] {
-		return inner_product(v.begin() + (vSize / 2), v.begin() + (vSize * 3 / 4), w.begin() + (vSize / 2), 0);
+		return inner_product(v.begin() + (vSize / 2), v.begin() + (vSize * 3 / 4), w.begin() + (vSize / 2), 0LL);
 		});
 
 	auto future4 = async([&] {
-		return inner_product(v.begin() + (vSize * 3 / 4), v.end(), w.begin() + (vSize * 3 / 4), 0);
+		return inner_product(v.begin() + (vSize * 3 / 4), v.end(), w.begin() + (vSize * 3 / 4), 0LL);
 		});
 
 	return __int64(future1.get()) + __int64(future2.get()) + __int64(future3.get()) + __int64(future4.get());
